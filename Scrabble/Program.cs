a95745997a43cf567abd653e1a7608be9c9ad336
Casey Hill <97810619+0art-vandelay0@users.noreply.github.com>
2023-07-12 12:09:50 -0700

    Build Main code

--- Scrabble/Program.cs
+++ Scrabble/Program.cs
@@ -7,7 +7,46 @@ namespace Scrabble.Models
     {
         public static void Main()
         {
-            //
+            Console.WriteLine("---------------------");
+            Console.WriteLine("     SCRABBLE!");
+            Console.WriteLine("--------------------- \n ");
+            Console.WriteLine("Enter any single word: ");
+            string word = Console.ReadLine();
+            int score = 0;
+
+            try
+            {
+                ScrabbleScore userScrabbleScore = new ScrabbleScore(word, score);
+                Console.WriteLine("Your score is: " + ScrabbleScore.GetScore(word));
+                TryAgain();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error = {0}" + ex.Message);
+                Console.WriteLine("Please enter a valid word in the form of letters only.");
+                Console.WriteLine("Try again...");
+                Main();
+            }
+        }
+
+        public static void TryAgain()
+        {
+            Console.WriteLine("Would you like to try another word? [Y/N]");
+            string answer = Console.ReadLine();
+            if (answer == "Y" || answer == "y")
+            {
+                Main();
+            }
+            else if (answer == "N" || answer == "n")
+            {
+                Console.WriteLine("Goodbye!");
+            }
+            else
+            {
+                Console.WriteLine("Please enter Y or N");
+                TryAgain();
+            }
         }
     }
 }
